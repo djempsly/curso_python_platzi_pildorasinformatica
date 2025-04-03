019678c4502ff838e35af7e311b03c799d8e1909
Jeffry Almonte Dely <djempsly120@gmail.com>
2025-04-03 12:29:23 -0400

    Agregando diccionario, matrix y slice

---
+++ diccionario_2.py
@@ -0,0 +1,3 @@
+mi_diccionario2 = {1:'uno', 2:'dos', 3:'tres'}
+
+print(mi_diccionario2[2])
---
+++ mislice.py
@@ -0,0 +1,11 @@
+a = [3, 4, 5, 6, 7, ]
+b= a
+print(a)
+print(b)
+b= a[:]
+print(b)
+a.append(9)
+c=a
+print(c)
+c = a.copy()
+print(c)
